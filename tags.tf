ec8fd1008e1780063c5d176b195fd4416b6b5693
lfeldman <lukasz.feldman@oracle.com>
2021-10-19 11:33:53 +0000

    Updated Random Id Tag

--- tags.tf
+++ tags.tf
@@ -9,7 +9,7 @@ resource "oci_identity_tag_namespace" "ArchitectureCenterTagNamespace" {
   provider       = oci.homeregion
   compartment_id = var.compartment_ocid
   description    = "ArchitectureCenterTagNamespace"
-  name           = "ArchitectureCenter\\deploy-oke-with-atp-in-oci"
+  name           = "ArchitectureCenter\\deploy-oke-with-atp-in-oci-${random_id.tag.hex}"
 
   provisioner "local-exec" {
     command = "sleep 10"
